e05c90960a62e631ec80939636bd361195970d82
sh-vasily <vasia.punk.rok@gmail.com>
2022-10-21 07:23:26 +0300

    fix up script

--- docker/mongo-scripts/utils.js
+++ docker/mongo-scripts/utils.js
@@ -1,32 +1,41 @@
 const shard01Nodes = [
-    "rs-shard-01/shard01-a:27017",
-    "rs-shard-01/shard01-b:27017",
-    "rs-shard-01/shard01-c:27017",
+    "shard01-a:27017",
+    "shard01-b:27017",
+    "shard01-c:27017",
 ];
 
 const shard02Nodes = [
-    "rs-shard-02/shard02-a:27017",
-    "rs-shard-02/shard02-b:27017",
-    "rs-shard-02/shard02-c:27017",
+    "shard02-a:27017",
+    "shard02-b:27017",
+    "shard02-c:27017",
 ];
 
 const shard03Nodes = [
-    "rs-shard-03/shard03-a:27017",
-    "rs-shard-03/shard03-b:27017",
-    "rs-shard-03/shard03-c:27017",
+    "shard03-a:27017",
+    "shard03-b:27017",
+    "shard03-c:27017",
 ];
 
 const allShards = [
-    ...shard01Nodes,
-    ...shard02Nodes,
-    ...shard03Nodes,
+    ...shard01Nodes.map(node => `rs-shard-01/${node}`),
+    ...shard02Nodes.map(node => `rs-shard-02/${node}`),
+    ...shard03Nodes.map(node => `rs-shard-03/${node}`),
 ];
 
-getReplicaSetConfig = (id, members, configsvr = false) => {
+
+getReplicaSetConfig = (id, rsMembers, configsvr = false) => {
+
+    let indexedMembers = rsMembers.map((member, index) => {
+        return {
+            _id: index,
+            host: member,
+        }
+    });
+
     return {
         _id: id,
         configsvr: configsvr,
         version: 1,
-        members: members
+        members: indexedMembers,
     };;
 };
